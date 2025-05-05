f551ea9c7320762de27214bdd3a84853b35b3958
nmetens <metens@pdx.edu>
2025-05-05 16:56:40 -0700

    creating more tables

--- create_players.sql
+++ create_players.sql
@@ -1,10 +1,10 @@
 -- Create the players table: --
 create schema if not exists nba;
 
-drop table if exists nba.Player;
+-- drop table if exists nba.Player;
 create table if not exists
 nba.Player (
-	player_id serial primary key,
+	player_id serial primary key, -- Primary Key
 	full_name varchar(30),
 	age int,
 	height varchar(10), -- 6'8"
@@ -19,3 +19,44 @@ insert into nba.Player
 	(full_name, age, height, country, position, active, debut_year)
 values
 ('Stephen Curry', 36, '62', 'USA', 'PG', 1, '2011-10-30');
+
+
+-- Creatig the Team table: --
+create table if not exists
+nba.Team (
+	team_id serial primary key, -- Primary Key
+	name varchar(50),
+	abbrev varchar(30),
+	city varchar(30),
+	coach varchar(30)
+);
+
+insert into nba.Team 
+	(name, abbrev, city, coach)
+values
+	('Golden State Warriors', 'GSW', 'San Francisco', 'Steve Kerr');
+
+-- Creating the Season table: --
+create table if not exists
+nba.Season (
+	season_id serial primary key, -- Primary Key
+	year_start date,
+	year_end date
+);
+
+-- Creating the Award table: --
+create table if not exists
+nba.Award (
+	award_id serial primary key, --Primary Key
+	name varchar(30),
+	description varchar(100)
+);
+
+-- Creating them PlayerAward table: --
+create table if not exists
+nba.PlayerAward (
+	player_id int references nba.Player(player_id), -- Foreign Key
+	award_id int references nba.Award(award_id), -- Foreign Key
+	season_id int references nba.Season(season_id), -- Foreign Key
+	primary key (player_id, award_id, season_id) -- Composite Key
+);
